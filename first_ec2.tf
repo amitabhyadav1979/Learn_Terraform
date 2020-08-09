a9dc503275caebb5af1bcd20fd8060b853b7c639
Amitabh Yadav <38934713+amitabhyadav1979@users.noreply.github.com>
2020-08-09 11:16:54 -0500

    Update first_ec2.tf

--- first_ec2.tf
+++ first_ec2.tf
@@ -1,8 +1,4 @@
-provider "aws" {
-  region     = "us-east-1"
-  access_key = "AKIA3JYLWBU6OXL6FE7J"
-  secret_key = "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"
-}
+
 
 resource "aws_instance" "myec2" {
     ami = "ami-0c5bf07e510b75b11"
